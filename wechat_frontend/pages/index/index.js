c27ed7e55fd83891841c6d9a359d18379a5b6203
Yexk <yexk@yexk.cn>
2018-07-12 01:29:24 +0800

    修复图标问题。学习小程序

--- wechat_frontend/pages/index/index.js
+++ wechat_frontend/pages/index/index.js
@@ -1,12 +1,32 @@
 //index.js
 //获取应用实例
 const app = getApp()
-console.log(app)
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
 Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    imgUrls: [
+      '/assets/imgs/home.png',
+      '/assets/imgs/cate.png',
+      '/assets/imgs/user.png'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+    tabs: ["选项一", "选项二", "选项三"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   },
   //事件处理函数
   bindViewTap: function() {
@@ -15,6 +35,15 @@ Page({
     })
   },
   onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    })
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -50,9 +79,12 @@ Page({
       hasUserInfo: true
     })
   },
+  onGotUserInfo: (data) => {
+    console.log(data);
+  },
   addParamsAA: function(){
     this.setData({
       aa: 'aa'
     })
-  }
+  },
 })
